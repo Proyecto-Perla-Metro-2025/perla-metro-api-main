5d2cb3a857886ca731e39e21af382e17d7c02927
JhonSnow <jhon.vallecilla@alumnos.ucn.cl>
2025-09-26 17:58:50 -0300

    feat: integrate get and create ticket feature

--- ApiMain/Program.cs
+++ ApiMain/Program.cs
@@ -1,6 +1,19 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+builder.Services.AddOcelot(builder.Configuration);
+
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
+builder.Logging.SetMinimumLevel(LogLevel.Debug);
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+await app.UseOcelot();
 
 app.Run();
